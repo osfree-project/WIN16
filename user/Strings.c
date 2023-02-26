de73831248579a5701151f87683190728170bd52
prokushev <yuri.prokushev@gmail.com>
2023-02-26 18:49:55 +0700

    String functions added to user.exe from kernel.exe

--- user/Strings.c
+++ user/Strings.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <ctype.h>
 
 #include <windows.h>
 
@@ -42,4 +43,99 @@ int WINAPI LoadString( HINSTANCE instance, UINT resource_id, LPSTR buffer, int b
     }
     FreeResource( hmem );
     return ret;
+}
+
+/***********************************************************************
+ *           lstrcmp   (USER.430)
+ */
+int WINAPI lstrcmp( LPCSTR str1, LPCSTR str2 )
+{
+  while (*str1 || *str2)
+    {
+      if (*str1 < *str2)
+        return -1;
+      else if (*str1 > *str2)
+        return 1;
+      str1 ++;
+      str2 ++;
+    }
+
+  return 0;
+}
+
+/***********************************************************************
+ *           AnsiUpper   (USER.431)
+ */
+
+LPSTR WINAPI AnsiUpper( LPSTR strOrChar )
+{
+    /* uppercase only one char if strOrChar < 0x10000 */
+    if (HIWORD(strOrChar))
+    {
+        char far *s = strOrChar;
+        while (*s)
+        {
+            *s = toupper(*s);
+            s++;
+        }
+        return strOrChar;
+    }
+    else return (LPSTR)toupper((char)strOrChar);
+}
+
+/***********************************************************************
+ *           AnsiLower   (USER.432)
+ */
+LPSTR WINAPI AnsiLower( LPSTR strOrChar )
+{
+    /* lowercase only one char if strOrChar < 0x10000 */
+    if (HIWORD(strOrChar))
+    {
+        char far *s = strOrChar;
+        while (*s)
+        {
+            *s = tolower(*s);
+            s++;
+        }
+        return strOrChar;
+    }
+    else return (LPSTR)tolower((char)strOrChar);
+}
+
+/***********************************************************************
+ *           AnsiNext   (USER.472)
+ */
+LPSTR WINAPI AnsiNext(LPCSTR lpchCurrentChar)
+{
+    if (!lpchCurrentChar)
+	return (LPSTR)0;
+
+    if (*lpchCurrentChar) {
+//	if ( IsDBCSLeadByte(*lpchCurrentChar) )
+//	    return (LPSTR)(lpchCurrentChar+2);
+//	else
+	    return (LPSTR)(lpchCurrentChar+1);
+    }
+    else
+	return (LPSTR)lpchCurrentChar;
+}
+
+/***********************************************************************
+ *           AnsiPrev   (USER.473)
+ */
+LPSTR WINAPI AnsiPrev(LPCSTR lpchStart, LPCSTR lpchCurrentChar)
+{
+    LPSTR lpPrev = (LPSTR)lpchStart;
+    LPSTR lpNext;
+
+    if (lpchStart == lpchCurrentChar)
+	return (LPSTR)lpchStart;
+
+//    if ( SetCodePage() ) {
+//	while ((lpNext = AnsiNext((LPCSTR)lpPrev)) != (LPSTR)lpchCurrentChar)
+//	    lpPrev = lpNext;
+//	return lpPrev;
+//    }
+
+    return (LPSTR)(lpchCurrentChar-1);
 }
