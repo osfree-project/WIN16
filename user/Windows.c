eb590a64c7af55992e9bb44d78035b4ba8cd5d8c
prokushev <yuri.prokushev@gmail.com>
2023-03-05 00:48:48 +0700

    CreateWindow added

--- user/Windows.c
+++ user/Windows.c
@@ -13,3 +13,15 @@ void WINAPI FillWindow( HWND hwndParent, HWND hwnd, HDC hdc, HBRUSH hbrush )
     DPtoLP( hdc, (LPPOINT)&rect, 2 );
     PaintRect( hwndParent, hwnd, hdc, hbrush, &rect );
 }
+
+/***********************************************************************
+ *		CreateWindow (USER.41)
+ */
+HWND WINAPI CreateWindow( LPCSTR className, LPCSTR windowName,
+                              DWORD style, int x, int y, int width,
+                              int height, HWND parent, HMENU menu,
+                              HINSTANCE instance, LPVOID data )
+{
+    return CreateWindowEx( 0, className, windowName, style,
+                             x, y, width, height, parent, menu, instance, data );
+}
