fd0e761669255956f9eb501b0fbce8fdb1272f27
prokushev <yuri.prokushev@gmail.com>
2023-06-22 13:56:51 +0700

    draft of LOGO with bitmap loading support.

--- drivers/display/logo/logo.asm
+++ drivers/display/logo/logo.asm
@@ -17,7 +17,7 @@
 			include	bios.inc
 			include	dos.inc
 
-GRAPH			equ	0		; Set to 1 for 320x200 VGA mode with logo
+GRAPH			equ	1		; Set to 1 for 320x200 VGA mode with logo
 
 code			segment
 			org	0h
@@ -78,6 +78,176 @@ Init:			push	ds
 		        mov     [CurrentVideoMode],al
 if			GRAPH
 			@SetMode 13h		;  13h 40x25 320x200 256 colors VGA 1
+
+    ; Process BMP file
+    call OpenFile
+    call ReadHeader
+    call ReadPalette
+    call CopyPal
+    call CopyBitmap
+    call CloseFile
+    jmp skip
+
+filename db 'logo.bmp',0
+filehandle dw ?
+Header db 54 dup (0)
+Palette db 256*4 dup (0)
+ScrLine db 320 dup (0)
+ErrorMsg db 'Error', 13, 10,'$'
+
+CloseFile:
+  mov  ah, 3Eh
+  mov  bx, [filehandle]
+  int  21h
+  ret
+
+OpenFile:
+
+    ; Open file
+
+    mov ah, 3Dh
+    xor al, al
+    mov dx, offset filename
+    int 21h
+
+    jc openerror
+    mov [filehandle], ax
+    ret
+
+    openerror:
+    mov dx, offset ErrorMsg
+    mov ah, 9h
+    int 21h
+    ret
+
+ReadHeader:
+
+    ; Read BMP file header, 54 bytes
+
+    mov ah,3fh
+    mov bx, [filehandle]
+    mov cx,54
+    mov dx,offset Header
+    int 21h
+    ret
+
+ReadPalette:
+
+    ; Read BMP file color palette, 256 colors * 4 bytes (400h)
+
+    mov ah,3fh
+    mov cx,400h
+    mov dx,offset Palette
+    int 21h
+    ret
+
+CopyPal:
+
+    ; Copy the colors palette to the video memory
+    ; The number of the first color should be sent to port 3C8h
+    ; The palette is sent to port 3C9h
+
+    mov si,offset Palette
+    mov cx,256
+    mov dx,3C8h
+    mov al,0
+
+    ; Copy starting color to port 3C8h
+
+    out dx,al
+
+    ; Copy palette itself to port 3C9h
+
+    inc dx
+    PalLoop:
+
+    ; Note: Colors in a BMP file are saved as BGR values rather than RGB.
+
+    mov al,[si+2] ; Get red value.
+    shr al,1 ; Max. is 255, but video palette maximal
+    shr al,1 ; Max. is 255, but video palette maximal
+
+    ; value is 63. Therefore dividing by 4.
+
+    out dx,al ; Send it.
+    mov al,[si+1] ; Get green value.
+    shr al,1
+    shr al,1
+    out dx,al ; Send it.
+    mov al,[si] ; Get blue value.
+    shr al,1
+    shr al,1
+    out dx,al ; Send it.
+    add si,4 ; Point to next color.
+
+    ; (There is a null chr. after every color.)
+
+    loop PalLoop
+    ret
+
+CopyBitmap:
+
+    ; BMP graphics are saved upside-down.
+    ; Read the graphic line by line (200 lines in VGA format),
+    ; displaying the lines from bottom to top.
+
+    mov ax, 0A000h
+    mov es, ax
+    mov cx,200
+    PrintBMPLoop:
+    push cx
+
+    ; di = cx*320, point to the correct screen line
+
+    mov di,cx
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    shl di,1
+    shl di,1
+    shl di,1
+    shl di,1
+    shl di,1
+    shl di,1
+    shl di,1
+    shl di,1
+    add di,cx
+
+    ; Read one line
+
+    mov ah,3fh
+    mov cx,320
+    mov dx,offset ScrLine
+    int 21h
+
+    ; Copy one line into video memory
+
+    cld 
+
+    ; Clear direction flag, for movsb
+
+    mov cx,320
+    mov si,offset ScrLine
+    rep movsb 
+
+    ; Copy line to the screen
+    ;rep movsb is same as the following code:
+    ;mov es:di, ds:si
+    ;inc si
+    ;inc di
+    ;dec cx
+    ;loop until cx=0
+
+    pop cx
+    loop PrintBMPLoop
+    ret
+
+skip:
+
+			if 0
                         push DS
                         mov  ax, 0A000H ;SegA000      { Screen segment }
 			mov es,ax
@@ -97,6 +267,7 @@ ll0:
                         dec  dx
                         jnz  ll0             ;{ draw all lines }
                         pop  DS
+			endif
 else
 			@SetMode 3
 			@DispStr Hello
