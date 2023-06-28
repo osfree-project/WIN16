c574b22f0422c7aed0776f6706b3498e29192277
prokushev <yuri.prokushev@gmail.com>
2023-06-28 09:09:35 +0700

    port revision added

--- dosx/hdpmi/hdpmi.mk
+++ dosx/hdpmi/hdpmi.mk
@@ -3,7 +3,7 @@
 PORT_NAME = dos$(SEP)hdpmi
 PORT_TYPE = git
 PORT_URL  = https://github.com/Baron-von-Riedesel/HX
-PORT_REV  = 
+PORT_REV  = v2.20
 #PORT_PATCHES  = attrib.diff
 
 !include $(%ROOT)tools/mk/port.mk
