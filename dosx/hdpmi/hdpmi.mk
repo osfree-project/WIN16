f9226c1cf8e2171400b346892cdacee09f509d81
prokushev <yuri.prokushev@gmail.com>
2023-06-28 09:01:51 +0700

    changed to port

---
+++ dosx/hdpmi/hdpmi.mk
@@ -0,0 +1,9 @@
+# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
+
+PORT_NAME = dos$(SEP)hdpmi
+PORT_TYPE = git
+PORT_URL  = https://github.com/Baron-von-Riedesel/HX
+PORT_REV  = 
+#PORT_PATCHES  = attrib.diff
+
+!include $(%ROOT)tools/mk/port.mk
