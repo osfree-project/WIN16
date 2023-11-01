d7ad2e1586930e6077d01a37f7cbdc3a3831f7cc
prokushev <yuri.prokushev@gmail.com>
2023-11-01 20:45:15 +0700

    taskman as submodule

--- programs/taskman/README.md
+++
@@ -1,5 +0,0 @@
-# Open source Tak Manager clone for windows 3.x
-
-This is clone of Windows 3.x Task Manager
-
-![](taskman.png)
--- programs/taskman/penwin.h
+++
@@ -1,9 +0,0 @@
-/*
- * Windows Task Manager
- *
- * Copyright 2023 by Yuri Prokushev
- *
- * osFree licence.
- */
-
-#define RPA_DEFAULT 1
