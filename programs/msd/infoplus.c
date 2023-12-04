59c187900ae1fef211ed646eb158b6aef142f443
prokushev <yuri.prokushev@gmail.com>
2023-12-04 11:58:08 +0700

    more port

--- programs/msd/infoplus.c
+++ programs/msd/infoplus.c
@@ -97,6 +97,78 @@ int wherey(void)
 	return xy.row;
 }
 
+#define FAR far
+#define SCROLL_UP 0
+#define SCROLL_DN 1
+#if !defined(COLORMODE)
+#define COLORMODE  ((*(char FAR *)0x0449) != 7)
+#define EXT_KBD    (*(char FAR *)0x0496 & 16)
+#define VIDPAGE    (*((unsigned char far *)0x0462))
+#define ROWSIZE    (*(int FAR *)0x044A)
+#define SCANLINES  ((int)*(char FAR*)0x0461)
+#define SCRBUFF    ((unsigned FAR *)((COLORMODE)?0xB8000000:0xB0000000))
+#define SCREENSEG  ((unsigned)((COLORMODE)?0xB800:0xB000))
+#define SCREENSIZE ((*(int FAR *)0x044C) >> 1)
+#define SCREENCOLS (*(int FAR *)0x044A)
+#define SCREENROWS ((*(char FAR *)0x0484)?1+(*(char FAR *)0x0484):25)
+#endif
+
+void scroll(int direction, int num_lines, int vattrib, int ulrow, int ulcomumn, int lrrow, int lrcolumn)
+{
+      union REGS regs;
+ 
+      /*
+            BH = attribute to be used on blank line
+            CH = row of upper left corner of scroll window
+            CL = column of upper left corner of scroll window
+            DH = row of lower right corner of scroll window
+            DL = column of lower right corner of scroll window
+      */
+ 
+      regs.h.al = (unsigned char)num_lines;
+      regs.h.bh = (unsigned char)vattrib;
+      regs.h.ch = (unsigned char)ulrow;
+      regs.h.cl = (unsigned char)ulcomumn;
+      regs.h.dh = (unsigned char)lrrow;
+      regs.h.dl = (unsigned char)lrcolumn;
+ 
+      if (direction == SCROLL_UP)
+            regs.h.ah = 0x06;
+      else  regs.h.ah = 0x07;
+ 
+      int86(0x10, &regs, &regs);
+}
+
+void GetCurPos(int *col, int *row)
+{
+      union REGS regs;
+
+      regs.h.ah = 0x03;
+      regs.h.bh = VIDPAGE;
+      int86(0x10, &regs, &regs);
+      *row = regs.h.dh;
+      *col = regs.h.dl;
+}
+
+int GetCurAtr(void)
+{
+      int row, col;
+      unsigned short chat;
+
+      GetCurPos(&col, &row);
+      chat = *((unsigned FAR *)MK_FP(SCREENSEG,
+            (row * SCREENCOLS + col) << 1));
+      return (chat >> 8);
+}
+
+void clreol(void)
+{
+      int row, col;
+
+      GetCurPos(&col, &row);
+      scroll(0, 0, GetCurAtr(), row, col, row, SCREENCOLS);
+}
+
 #include "ifprun.ic"
 
 void main(int argc, char* argv[])
