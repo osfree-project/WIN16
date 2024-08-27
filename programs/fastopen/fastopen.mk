6bc74697bb4b9739d7e72b830948849d9a797632
prokushev <yuri.prokushev@gmail.com>
2024-08-27 16:59:36 +0700

    some patches for FASTOPEN

--- programs/fastopen/fastopen.mk
+++ programs/fastopen/fastopen.mk
@@ -3,6 +3,6 @@ PORT_NAME = win16$(SEP)fastopen
 PORT_TYPE = git
 PORT_URL  = https://github.com/microsoft/MS-DOS
 PORT_REV  = main
-PORT_PATCHES  = 
+PORT_PATCHES  = fastopen.diff
 
 !include $(%ROOT)tools/mk/port.mk
