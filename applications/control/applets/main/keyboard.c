d3325d83d46c3da4247562bd0da7a788cb740198
prokushev <yuri.prokushev@gmail.com>
2026-06-08 14:50:38 +0700

    Implementation of Keyboard applet via ControlPanelInfo

--- applications/control/applets/main/keyboard.c
+++ applications/control/applets/main/keyboard.c
@@ -1,38 +1,96 @@
+/*
+ * keyboard.c -- Keyboard applet dialog procedure (Windows 3.0)
+ */
+
 #include <windows.h>
 #include <dos.h>
 #include <string.h>
 #include <stdlib.h>
 #include "main.h"
 
-/* ============================================================
- *  Keyboard
- * ============================================================ */
+/* ControlPanelInfo constants */
+#define CPI_GETKEYBOARDSPEED 10
+#define CPI_SETKEYBOARDSPEED 11
+
+/* Prototype (недокументированная функция из USER) */
+VOID FAR PASCAL ControlPanelInfo(int nInfoType, WORD wData, LPSTR lpBuffer);
+
+static int   g_keyboardSpeed = 31;   /* текущая скорость (1..32) */
+static int   g_origSpeed;
+static HWND  hSpeedScroll = NULL;
+
 BOOL CALLBACK KeyboardDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg) {
-    case WM_INITDIALOG: {
-        int delay, speed;
-        SystemParametersInfo(SPI_GETKEYBOARDDELAY, 0, &delay, 0);
-        SystemParametersInfo(SPI_GETKEYBOARDSPEED, 0, &speed, 0);
-        SetScrollRange(GetDlgItem(hDlg, IDC_KB_DELAY), SB_CTL, 0, 3, FALSE);
-        SetScrollPos(GetDlgItem(hDlg, IDC_KB_DELAY), SB_CTL, delay, TRUE);
-        SetScrollRange(GetDlgItem(hDlg, IDC_KB_RATE), SB_CTL, 0, 31, FALSE);
-        SetScrollPos(GetDlgItem(hDlg, IDC_KB_RATE), SB_CTL, speed, TRUE);
+    case WM_INITDIALOG:
+    {
+        WORD wBuf;
+
+        /* Получаем текущую скорость клавиатуры */
+        ControlPanelInfo(CPI_GETKEYBOARDSPEED, 0, (LPSTR)&wBuf);
+        g_keyboardSpeed = wBuf;
+        if (g_keyboardSpeed < 1)  g_keyboardSpeed = 1;
+        if (g_keyboardSpeed > 32) g_keyboardSpeed = 32;
+        g_origSpeed = g_keyboardSpeed;
+
+        hSpeedScroll = GetDlgItem(hDlg, IDC_KB_SPEED);
+        SetScrollRange(hSpeedScroll, SB_CTL, 1, 32, FALSE);
+        SetScrollPos(hSpeedScroll, SB_CTL, g_keyboardSpeed, TRUE);
         return TRUE;
     }
-    case WM_HSCROLL: {
-        int id = GetWindowWord((HWND)lParam, GWW_ID);
-        int pos = HIWORD(wParam);
-        if (id == IDC_KB_DELAY)
-            SystemParametersInfo(SPI_SETKEYBOARDDELAY, pos, NULL, 0);
-        else if (id == IDC_KB_RATE)
-            SystemParametersInfo(SPI_SETKEYBOARDSPEED, pos, NULL, 0);
+
+    case WM_HSCROLL:
+    {
+        int code = wParam;
+        int pos = LOWORD(lParam);
+
+        if ((HWND)HIWORD(lParam) != hSpeedScroll)
+            return FALSE;
+
+        switch (code) {
+        case SB_THUMBTRACK:
+        case SB_THUMBPOSITION:
+            g_keyboardSpeed = pos;
+            break;
+        case SB_LINELEFT:
+            if (g_keyboardSpeed > 1) g_keyboardSpeed--;
+            break;
+        case SB_LINERIGHT:
+            if (g_keyboardSpeed < 32) g_keyboardSpeed++;
+            break;
+        case SB_PAGELEFT:
+            g_keyboardSpeed -= 5;
+            if (g_keyboardSpeed < 1) g_keyboardSpeed = 1;
+            break;
+        case SB_PAGERIGHT:
+            g_keyboardSpeed += 5;
+            if (g_keyboardSpeed > 32) g_keyboardSpeed = 32;
+            break;
+        default:
+            return FALSE;
+        }
+        SetScrollPos(hSpeedScroll, SB_CTL, g_keyboardSpeed, TRUE);
+        ControlPanelInfo(CPI_SETKEYBOARDSPEED, g_keyboardSpeed, NULL);
         return TRUE;
     }
+
     case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-            EndDialog(hDlg, LOWORD(wParam));
-        return TRUE;
+        switch (LOWORD(wParam)) {
+        case IDOK:
+        {
+            char buf[16];
+            wsprintf(buf, "%d", g_keyboardSpeed);
+            WriteProfileString("windows", "KeyboardSpeed", buf);
+            EndDialog(hDlg, IDOK);
+            return TRUE;
+        }
+        case IDCANCEL:
+            /* Восстанавливаем исходную скорость и закрываем */
+            ControlPanelInfo(CPI_SETKEYBOARDSPEED, g_origSpeed, NULL);
+            EndDialog(hDlg, IDCANCEL);
+            return TRUE;
+        }
+        break;
     }
     return FALSE;
 }
