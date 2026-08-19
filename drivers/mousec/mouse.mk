dc50e8fddb8d0443e02194037708dddb95a6918e
prokushev <yuri.prokushev@gmail.com>
2026-08-19 21:10:16 +0700

    repo url fix

--- drivers/mousec/mouse.mk
+++ drivers/mousec/mouse.mk
@@ -2,7 +2,7 @@
 
 PORT_NAME = win16$(SEP)mouse
 PORT_TYPE = git
-PORT_URL  = https://git.javispedro.com/cgit/vbmouse.git/
+PORT_URL  = https://git.javispedro.com/vbmouse.git #https://git.javispedro.com/cgit/vbmouse.git/
 PORT_REV  = 
 #PORT_PATCHES  = attrib.diff
 
